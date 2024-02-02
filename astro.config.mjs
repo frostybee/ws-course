d7312a93a5697fe23575e54666cc765ab0e4b242
frostybee <rsleiman2@gmail.com>
2024-02-01 23:38:17 -0500

    Add gh action for deploying Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,26 +3,28 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://frostybee.github.io',
+	base: '/ws-course',
 	integrations: [
 		starlight({
 			title: 'My Docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/frostybee/ws-course',
 			},
 			customCss: [
 				// Relative path to your custom CSS file
 				'./src/styles/custom.css',
-			  ],
+			],
 			// Set English as the default language for this site.
-			defaultLocale: 'en',			
-			sidebar: [   				
+			defaultLocale: 'en',
+			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Information', link: '/guides/' },
 					],
-				},				
+				},
 				{
 					label: 'Lectures',
 					autogenerate: { directory: 'lectures' },
