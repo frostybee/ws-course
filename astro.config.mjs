80bbb7ef47b239c70d47fbd1237b7555697d0adf
frostybee <rsleiman2@gmail.com>
2024-08-14 20:11:42 -0400

    Add external link component and a list of public APIs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,61 +1,68 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://frostybee.github.io",
   base: "/ws-course",
-  integrations: [
-    starlight({
-      title: "420-511-VA Web Services",
-      social: {
-        github: "https://github.com/frostybee/ws-course",
-      },
-      customCss: [
-        // Relative path to your custom CSS file
-        "./src/styles/custom.css",
-      ],
-      // Set English as the default language for this site.
-      defaultLocale: "en",
-      sidebar: [
-        {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Information", link: "/guides/" },
-          ],
-        },
-        {
-          label: "Lectures",
-          autogenerate: { directory: "lectures" },
-        },
-        {
-          label: "Resources",
-          items: [
-            { label: "Resource Design", link: "/resources/resources" },
-            {
-              label: "Collection Resources",
-              link: "/resources/collection-resources",
-            },
-            {
-              label: "Sub-Resources",
-              link: "/resources/relationships-and-sub-resources",
-            },
-            {
-              label: "Composite Resource",
-              link: "/resources/composite-resource",
-            },
-          ],
-        },
-        {
-          label: "HTTP",
-          autogenerate: { directory: "http" },
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
-      ],
-    }),
-  ],
+  integrations: [starlight({
+    title: "420-511-VA Web Services",
+    social: {
+      github: "https://github.com/frostybee/ws-course"
+    },
+    customCss: [
+    // Relative path to your custom CSS file
+    "./src/styles/custom.css"],
+    // Set English as the default language for this site.
+    defaultLocale: "en",
+    sidebar: [{
+      label: "Guides",
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: "Information",
+        link: "/guides/"
+      }]
+    }, {
+      label: "Lectures",
+      autogenerate: {
+        directory: "lectures"
+      }
+    }, {
+      label: "REST Resources",
+      items: [{
+        label: "Resource Design",
+        link: "/resources/resources"
+      }, {
+        label: "Collection Resources",
+        link: "/resources/collection-resources"
+      }, {
+        label: "Sub-Resources",
+        link: "/resources/relationships-and-sub-resources"
+      }, {
+        label: "Composite Resource",
+        link: "/resources/composite-resource"
+      }]
+    }, {
+      label: "HTTP",
+      autogenerate: {
+        directory: "http"
+      }
+    }, {
+      label: "Extra",
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: "Public API",
+        link: "/extra/public-apis"
+      }]
+    }, {
+      label: "Reference",
+      autogenerate: {
+        directory: "reference"
+      }
+    }]
+  }), mdx()]
 });
