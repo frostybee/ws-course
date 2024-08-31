454decfcce38ff0a396b0e342bea0b57948f1ea3
frostybee <rsleiman2@gmail.com>
2024-08-30 22:36:06 -0400

    Add image zooming plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,37 +3,12 @@ import { defineConfig } from "astro/config";
 import react from '@astrojs/react'
 import mdx from "@astrojs/mdx";
 import starlightLinksValidator from 'starlight-links-validator'
+import starlightImageZoom from 'starlight-image-zoom'
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://frostybee.github.io",
   base: "/ws-course",
-  markdown: {
-    shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://shiki.style/themes
-      theme: 'aurora-x',
-      // Alternatively, provide multiple themes
-      // See note below for using dual light/dark themes
-      // themes: {
-      //   light: 'github-light',
-      //   dark: 'github-dark',
-      // },
-      // Disable the default colors
-      // https://shiki.style/guide/dual-themes#without-default-color
-      // (Added in v4.12.0)
-      defaultColor: false,
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://shiki.style/languages
-      langs: ['php', 'json'],
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-      // Add custom transformers: https://shiki.style/guide/transformers
-      // Find common transformers: https://shiki.style/packages/transformers
-      transformers: [],
-    },
-  },
   integrations: [
     starlight(
       {
@@ -47,6 +22,9 @@ export default defineConfig({
           // Relative path to your custom CSS file
           "./src/styles/custom_styles.css",
         ],
+        plugins: [
+          starlightImageZoom()
+        ],
         // plugins: [
         //   starlightLinksValidator({
         //     errorOnFallbackPages: false,
@@ -54,12 +32,18 @@ export default defineConfig({
         // ],
         // @doc: https://expressive-code.com/installation/
         expressiveCode: {
+          defaultProps: {
+            wrap: true
+          },
           styleOverrides: {
             borderColor: 'transparent',
             borderRadius: 'var(--border-radius)',
+            // frames: {
+            //   shadowColor: '#e0f7fa',
+            // }
           },
           //@themes: https://expressive-code.com/guides/themes/
-          themes: ['aurora-x', 'catppuccin-latte'],
+          themes: ['dracula', 'catppuccin-latte'],
         },
         components: {
           Header: './src/components/docs/Header.astro',
